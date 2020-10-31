049e762280c1d9a01c9ed6637070c87908382a0e
Osman Koç <info@osmkoc.com>
2020-11-01 02:48:12 +0300

    Yeni sesler eklendi & Ses seçme dropdown eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool isStart = false;
+  var isStart = false;
   var assetsAudioPlayer = AssetsAudioPlayer();
+
+  int selectedVoiceIndex = 0;
+  var voiceNames = <String>[
+    'Düdük sesi',
+    'Kedi sesi',
+    'Siren sesi',
+    'Mors alfabesi yardım sesi'
+  ];
+  var voiceUrls = <String>[
+    'assets/audios/referee-whistle.mp3',
+    'assets/audios/cat-meow.mp3',
+    'assets/audios/siren.mp3',
+    'assets/audios/morse-sos.mp3'
+  ];
+
+  void playVoice() {
+    if (isStart) {
+      assetsAudioPlayer.open(
+        Audio(voiceUrls[selectedVoiceIndex]),
+        autoStart: true,
+        showNotification: false,
+        loopMode: LoopMode.single,
+      );
+    } else {
+      assetsAudioPlayer.stop();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,26 +70,16 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 10.0)),
+            //Padding(padding: EdgeInsets.only(top: 10.0)),
             ButtonTheme(
-              height: 250,
-              minWidth: 250,
+              padding: EdgeInsets.all(50.0),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(100)),
               child: RaisedButton(
                 color: isStart ? Colors.grey : Colors.red,
                 onPressed: () => setState(() {
                   isStart = !isStart;
-                  if (isStart) {
-                    assetsAudioPlayer.open(
-                      Audio("assets/audios/referee-whistle.mp3"),
-                      autoStart: true,
-                      showNotification: false,
-                      loopMode: LoopMode.single,
-                    );
-                  } else {
-                    assetsAudioPlayer.stop();
-                  }
+                  playVoice();
                 }),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -77,18 +94,38 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
             ),
+            Container(
+              padding: EdgeInsets.all(5.0),
+              child: DropdownButton<String>(
+                value: voiceNames[selectedVoiceIndex],
+                items: voiceNames.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value, style: TextStyle(fontSize: 16)),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedVoiceIndex = voiceNames.indexOf(value);
+                    isStart = false;
+                    playVoice();
+                  });
+                },
+                style: TextStyle(color: Colors.black87, fontSize: 20),
+              ),
+            ),
             Text(
               '\n\nYardım için düdüğe basın.\nDurdurmak için tekrar basın.',
               textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
             ),
             Padding(padding: EdgeInsets.only(top: 50.0)),
             Text(
-              "v1.0.0\n© 2020  Osman KOÇ",
+              "© 2020 Osman KOÇ",
               textAlign: TextAlign.center,
               style: TextStyle(
                 color: Colors.black87,
-                fontSize: 11.0,
+                fontSize: 10.0,
               ),
             ),
           ],
